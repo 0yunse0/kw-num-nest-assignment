df0a7e55225bc0ca77fb66fc5cbbc81e3e6d2062
0yunse0 <yunny9624@naver.com>
2024-11-05 15:39:37 +0900

    add prime and factorial

--- lib.js
+++ lib.js
@@ -2,12 +2,25 @@ function avg(numbers) {
     let s = numbers.reduce((prev, curr) => prev + curr, 0);
     return s / numbers.length;
 }
+
 function prime(num) {
-    return false;
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
 }
+
 function factorial(num) {
-    return -1;
+    if (num < 0) return -1;
+    if (num === 0) return 1;
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+        result *= i;
+    }
+    return result;
 }
+
 module.exports = {
     avg,
     prime,
