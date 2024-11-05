60ac2992518f8ddde57234a759e9d4ee94631e52
0yunse0 <yunny9624@naver.com>
2024-11-05 15:45:50 +0900

    add test

--- lib.test.js
+++ lib.test.js
@@ -6,3 +6,30 @@ expect(lib.avg([3, 5, 7])).toBe(5);
 test("avg([-5, 5]) should be 0", () => {
 expect(lib.avg([-5, 5])).toBe(0);
 });
+
+//test prime
+test("prime(7) should be true", () => {
+    expect(lib.prime(7)).toBe(true);
+});
+
+test("prime(-7) should be false", () => {
+    expect(lib.prime(-7)).toBe(false);
+});
+
+test("prime(10) should be false", () => {
+    expect(lib.prime(10)).toBe(false);
+});
+
+
+//test factorial
+test("factorial(5) should be 120", () => {
+    expect(lib.factorial(5)).toBe(120);
+});
+
+test("factorial(-5) should be -1", () => {
+    expect(lib.factorial(-5)).toBe(-1);
+});
+
+test("factorial(0) should be 1", () => {
+    expect(lib.factorial(0)).toBe(1);
+});
