93eae19b5ca5cdf32b28b6ad7a658cf8549ae1a5
0yunse0 <yunny9624@naver.com>
2024-11-05 10:43:43 +0900

    switch-case

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log(process.argv);
+let command = process.argv[2];
+
+let numbers = process.argv
+    .slice(3, process.argv.length)
+    .map((n) => parseFloat(n));
+
+let result;
+switch (command) {
+    case "avg":
+        break;
+    case "prime":
+        break;
+    case "factorial":
+        break;
+    default:
+        console.log("Wrong command!");
+        process.exit(1);
+}
+console.log(result);
